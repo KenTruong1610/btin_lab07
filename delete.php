80b1e21d3be5ff26cfb327e20b6f6948590ec731
TunaKiet <tuankietpro167@gmail.com>
2025-04-27 21:07:24 +0700

    Third

--- delete.php
+++ delete.php
@@ -10,18 +10,25 @@ $thoigian="2025-04-20 18:07:43";
 $id_bantin=27;
 $id_docgia=1;
 
-// Giá trị cố định của câu 2
+// Giá trị cố định của câu thứ 2
 $id_binhluan=51;
 $tieude="Chờ đợi sản phẩm đột phá!";
 
+// Giá trị cố định của câu thứ 3
+$id_nguoidung=7;
+$ten_danhmuc = "Lịch Sử";
 taoKetNoi($link);
 
 
 //Câu truy vấn DELETE thứ 2
 $sql_delete="DELETE FROM tbl_binhluan
              WHERE id_binhluan=$id_binhluan AND tieude='$tieude'";
+
+//Câu truy vấn DELETE thứ 3
+$sql_3="DELETE FROM tbl_danhmuc
+        WHERE id_nguoidung=$id_nguoidung AND ten_danhmuc='$ten_danhmuc'";
 // Thực thi truy vấn
-if (chayTruyVanKhongTraVeDL($link, $sql_delete)) {
+if (chayTruyVanKhongTraVeDL($link, $sql_3)) {
     $thongbao = "Xóa bình luận '$tieude' thành công!";
 } else {
     $thongbao = "Xóa bình luận thất bại!";
